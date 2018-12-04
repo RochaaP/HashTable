a329cda3ad9db8eb6ff3b5daaa441fa472a21dd6
RochaaP <rochpathiraja@gmail.com>
2018-12-04 10:52:12 +0530

    Interface

---
+++ Src/HashTable.java
@@ -0,0 +1,18 @@
+
+interface HashTable {
+    /* insert the given key to a open hash tabled.
+     * With each key you have a count of how many times it 
+     * was inserted 
+     */
+    public void insert(String key);
+
+    /* given the key return the number of times it was inserted 
+     * to the table 
+     */
+    public int search(String key);
+
+
+
+}
+
+    
